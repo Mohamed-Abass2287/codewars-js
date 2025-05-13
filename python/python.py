8fada2fc6c627289b67a7d777c3195007a5a2cb7
mohamed abass tawana <aminabass987@gmail.com>
2025-05-13 12:05:24 +0300

    niliongeza kitu

--- python/python.py
+++ python/python.py
@@ -1,5 +1,17 @@
-# filter list
-def filter_list(l):
-    # return a new list withthe string filtered out
-    return [item for item in l if isinstance(item, int)]
-print(filter_list([1, 2, 'a', 'b',5]))
+# # filter list
+# def filter_list(l):
+#     # return a new list withthe string filtered out
+#     return [item for item in l if isinstance(item, int)]
+# print(filter_list([1, 2, 'a', 'b',5]))
+
+
+def maskify(cc):
+    return '#' *(len(cc) -4) + cc[-4:]
+
+# mfano used"
+print (maskify('45564636536'))
+print (maskify('123'))
+
+
+
+
